c2d67f7da20e5df2e3de451debbdbe037d6a9150
planeset94 <planeset94@gmail.com>
2021-07-12 16:20:15 +0200

    add MovieController & changed route struc

---
+++ laravel/resources/views/layout/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title', 'Home')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <header id="site-header">
+        <h1>Ciao</h1>
+    </header>
+
+
+    <main id="site-main">
+        @yield('content')
+    </main>
+
+
+
+    <footer id="site-footer">
+
+    </footer>
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ laravel/resources/views/movies/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('title', 'Home Movies')
+@section('content')
+
+    <p>Io sono il Main</p>
+
+
+
+@endsection
